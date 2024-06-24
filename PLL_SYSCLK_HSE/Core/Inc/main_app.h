b5754ad1e173fc1fae0ab0496314dec8b1bdba2a
kssachin11 <kssachin11@gmail.com>
2024-06-24 11:13:42 +0200

    SYSCLK CONFIG WITH HSE INPUT

---
+++ PLL_SYSCLK_HSE/Core/Inc/main_app.h
@@ -0,0 +1,15 @@
+/*
+ * main_app.h
+ *
+ *  Created on: Jun 24, 2024
+ *      Author: Sachin K S
+ */
+
+#ifndef INC_MAIN_APP_H_
+#define INC_MAIN_APP_H_
+
+#define SYS_CLOCK_FREQ_50_MHZ		50
+#define SYS_CLOCK_FREQ_84_MHZ		84
+#define SYS_CLOCK_FREQ_120_MHZ	120
+
+#endif /* INC_MAIN_APP_H_ */
